360cd41099d5b6340c68077da240968fb8b0bad7
Nikolaj Bech Andersen <nikolaj.bech@live.dk>
2024-11-02 13:22:42 +0100

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        Test
+      </main>
+    </HydrateClient>
+  );
+}
